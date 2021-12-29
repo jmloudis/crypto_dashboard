c89fda5c47fea91b247de7774eb61da05e6c836b
jordan <16jloudis@gmail.com>
2021-12-28 20:54:41 -0500

    Added a Navbar for the application

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import CurrencyConverter from "./CurrencyConverter";
+
+
+function NavBar() {
+
+    return (
+        <div>
+            <ul>
+                <li><a href="default.asp">Home</a></li>
+                <li><a href="news.asp">News</a></li>
+                <li><a href="contact.asp">Contact</a></li>
+                <li><a href="about.asp">About</a></li>
+            </ul>
+        </div>
+    );
+
+
+}
+
+export default NavBar;
